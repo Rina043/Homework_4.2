7b58566e591d25a3c484cad3686ea28e23e98e2c
Ekaterina Shipkova <katerinashipkova@gmail.com>
2020-10-17 13:49:43 +0400

    Tests fastestroute added

--- src/test/java/ru/netology/manager/AirRouteManagerTest.java
+++ src/test/java/ru/netology/manager/AirRouteManagerTest.java
@@ -2,7 +2,10 @@ package ru.netology.manager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.AirRoute;
+import ru.netology.domain.AirRouteByTimeAscComparator;
 import ru.netology.repository.AirRouteRepository;
+
+import java.util.Comparator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,38 +30,49 @@ class AirRouteManagerTest {
         String airportFrom = "VKO";
         String airportTo = "PAR";
 
-        AirRoute[] expected = new AirRoute[0];
-        AirRoute[] actual = manager.findAll(airportFrom, airportTo);
+        AirRoute[] expected = new AirRoute[]{};
+        AirRoute[] actual = manager.findAllPriceSort(airportFrom, airportTo);
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldNotFoundFirst() {
+    public void shouldNotFoundOne() {
         String airportFrom = "SVO";
         String airportTo = "PAR";
 
-        AirRoute[] expected = new AirRoute[0];
-        AirRoute[] actual = manager.findAll(airportFrom, airportTo);
+        AirRoute[] expected = new AirRoute[]{};
+        AirRoute[] actual = manager.findAllPriceSort(airportFrom, airportTo);
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldNotFoundSecond() {
+    public void shouldNotFoundTwo() {
         String airportFrom = "PAR";
         String airportTo = "CDG";
 
-        AirRoute[] expected = new AirRoute[0];
-        AirRoute[] actual = manager.findAll(airportFrom, airportTo);
+        AirRoute[] expected = new AirRoute[]{};
+        AirRoute[] actual = manager.findAllPriceSort(airportFrom, airportTo);
         assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldFoundAll() {
+    public void shouldFoundAllPriceSort() {
         String airportFrom = "SVO";
         String airportTo = "CDG";
 
         AirRoute[] expected = new AirRoute[]{second, forth};
-        AirRoute[] actual = manager.findAll(airportFrom, airportTo);
+        AirRoute[] actual = manager.findAllPriceSort(airportFrom, airportTo);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFoundAllTimeSort() {
+        String airportFrom = "SVO";
+        String airportTo = "CDG";
+        Comparator<AirRoute> comparator = new AirRouteByTimeAscComparator();
+
+        AirRoute[] expected = new AirRoute[]{second, forth};
+        AirRoute[] actual = manager.findAllTimeSort(airportFrom, airportTo, comparator);
         assertArrayEquals(expected, actual);
     }
 
